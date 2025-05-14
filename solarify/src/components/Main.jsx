805631a346d834330265a8e956ec62175a6fd03b
MuhammadSufian98 <sufianhassan98@gmail.com>
2025-05-14 12:44:55 +0500

    `Refactor Main component and update assets, styles and layout`

--- solarify/src/components/Main.jsx
+++ solarify/src/components/Main.jsx
@@ -1,20 +1,37 @@
 import React from "react";
+import "./Main.css";
+import solarImage from "../assets/home/solarBanner.png";
+import solarImage2 from "../assets/home/solarBanner2.jpg";
 
 function Main() {
   return (
-    <div>
+    <div className="MainOutter">
       <div>
-        <div>
-          <h1>PATH TO ENERGY.</h1>
-          <p>
-            This renewable energy source is abundant and sustainable, we
-            providing a friendly alternative to traditional fossil fuels.
-          </p>
+        <div className="HeadingDiv">
+          <h1 className="Heading">PATH TO ENERGY.</h1>
+          <div>
+            <p className="Paragraph">
+              This renewable energy source is abundant and sustainable, we
+              providing a friendly alternative to traditional fossil fuels.
+            </p>
+            <button className="DicoverBTN">Discover More</button>
+          </div>
         </div>
-        <div>
-          <img src="../assets/solarBanner.png" alt="Solar" />
-          <img src="../assets/solarBanner2.jpg" alt="Solar2" />
-          <button>Discover More</button>
+        <div className="ImageDiv">
+          <div>
+            <img src={solarImage2} alt="Solar2" className="MainImages2" />
+            <div>
+              <div>
+                <h1>200+</h1>
+                <p>Client Projects Completed.</p>
+              </div>
+              <div>
+                <h1>200+</h1>
+                <p>Client Projects Completed.</p>
+              </div>
+            </div>
+          </div>
+          <img src={solarImage} alt="Solar" className="MainImages" />
         </div>
         <div></div>
       </div>
