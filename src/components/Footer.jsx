e0ef792e534fde76a07ba2069bc9468005d4be2f
malikumair687 <malikumairbwp786@gmail.com>
2025-05-16 13:08:50 +0500

    Footer Set

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,19 +10,32 @@ import { MdArrowOutward } from "react-icons/md";
 
 const Footer = () => {
   return (
-<footer className="w-full   relative z-10 overflow-visible">
+
+    
+<footer className="w-full  bg-gray-100 pt-20 flex flex-col bottom-0  relative z-10 overflow-visible">
+
+
+{/* <!-- ========== Start Black Section ========== --> */}
 
   {/* Black Banner Section */}
-  <div className="absolute -top-90 left-1/2 z-50 -translate-x-1/2 bg-black h-100 text-white w-[90%] md:w-[70%] rounded-xl p-6 md:p-10 shadow-xl">
+  <div className=" bg-black h-100 text-white overflow-hidden w-[90%] md:w-[70%] rounded-xl p-6 md:p-10 shadow-xl">
     <div className="relative">
+
+      <div className="absolute rotate-20 -bottom-110 hidden sm:block  w-60 overflow-hidden h-50">
+        <img className="w-full h-full bg-cover overflow-hidden  bg-center" src="/public/About/FutSolar.webp" alt="" />
+      </div>
+      <div className="absolute rotate-12 hidden sm:block  -top-20 -right-5 bottom-0  w-60 overflow-hidden h-50">
+        <img className="w-full h-full bg-cover overflow-hidden  bg-center" src="/public/About/FutSolar.webp" alt="" />
+      </div>
       <div className="flex justify-between items-start gap-10 ">
       {/* Left Side Text */}
-      <div className="max-w-md flex sm:flex-row flex-col  absolute top-0 z-10">
+      <div className="max-w-xl flex sm:flex-row flex-col  absolute top-0 z-10">
         <p className="text-lg mb-2 ">
           Do you have questions about our services or want to learn more
           about sustainable living?
         </p>
-        <h1 className="text-[50px]  md:text-[80px] font-semibold leading-none pt-5">GET IN</h1>
+        {/* i need this below text in a single line  */}
+        <h1 className="text-[50px]  md:text-[100px] whitespace-nowrap font-semibold leading-none pt-5">GET IN</h1>
       </div>
 
       {/* Right Side Button */}
@@ -40,12 +53,15 @@ const Footer = () => {
     </div>
     </div>
   </div>
+    
+{/* <!-- ========== End Black Section ========== --> */}
 
+{/* <!-- ========== Start Footer content Section ========== --> */}
   {/* Footer Content */}
-  <div className="flex flex-col bg-gray-100 sm:flex-row w-[90%] mx-auto justify-between items-center px-4 pt-20 pb-20 space-y-6 sm:space-y-0">
+  <div className="flex flex-col  sm:flex-row w-[90%] mx-auto justify-between items-center px-4 pt-10 pb-10 space-y-6 sm:space-y-0">
     {/* Navigation Links */}
     <nav className="w-full sm:w-auto">
-      <ul className="grid grid-cols-2 sm:grid-cols-3 gap-3 text-sm sm:text-base font-semibold text-gray-700 text-center sm:text-left">
+      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-7 text-sm sm:text-base font-semibold text-gray-700 text-center sm:text-left">
         <li><Link to="/home" className="hover:text-blue-600 transition">Home</Link></li>
         <li><Link to="/project" className="hover:text-blue-600 transition">Project</Link></li>
         <li><Link to="/service" className="hover:text-blue-600 transition">Service</Link></li>
@@ -71,6 +87,14 @@ const Footer = () => {
       </p>
     </div>
   </div>
+
+{/* <!-- ========== End Footer content Section ========== --> */}
+
+
+
+
+
+
 </footer>
 
   );
